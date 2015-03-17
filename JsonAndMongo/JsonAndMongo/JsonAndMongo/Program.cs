a394c98ae2f21f5b0d5d2738a52795695fa0858c
venconi91 <vencislav_911115@abv.bg>
2015-03-17 18:08:29 +0200

    creating json files with database first works tested with sample data from zip files

---
+++ JsonAndMongo/JsonAndMongo/JsonAndMongo/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonAndMongo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sm = new SupermarketEntities();
+
+            var groups = sm.Sales.GroupBy(p => p.ProductID);
+
+            foreach (var group in groups)
+            {
+                string vendorName = sm.Products.Find(group.Key).Vendor.Vendor_Name;
+                //Console.WriteLine(vendorName);
+                string productName = sm.Products.Find(group.Key).Product_Name;
+                //Console.WriteLine(productName);
+                decimal incomes = (decimal)group.Sum(p => p.PriceSum);
+                //Console.WriteLine(incomes);
+                int quantitySold = (int)group.Sum(p => p.Quantity);
+                //Console.WriteLine(quantitySold);
+                //Console.WriteLine();
+                int id = (int)group.Key;
+                generateJson(id, productName, vendorName, quantitySold, incomes);
+
+            }
+        }
+
+        private static void generateJson(int id, string productName, string vendorName, int totalQuantity, decimal incomes)
+        {
+            var report = new SalesByProductReports(id, productName, vendorName, totalQuantity, incomes);
+
+            string serializedReport = JsonConvert.SerializeObject(report, Formatting.Indented);
+
+            ExportJsonToFile(serializedReport, id);
+        }
+
+        private static void ExportJsonToFile(string json, int id)
+        {
+            File.WriteAllText(@"../../JsonReports/" + id + @".json", json);
+        }
+    }
+}
---
+++ JsonAndMongo/JsonAndMongo/JsonAndMongo/SalesByProductReports.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonAndMongo
+{
+    public class SalesByProductReports
+    {
+        public SalesByProductReports(int id, string productName, string vendorName, int totalQuantity, decimal incomes)
+        {
+            this.Id = id;
+            this.ProductName = productName;
+            this.VendorName = vendorName;
+            this.Quantity = totalQuantity;
+            this.Incomes = incomes;
+        }
+
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string VendorName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Incomes { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("product-id: {0}, product-name: {1}, vandor-name: {2}, total-quantity-sold: {3}, total-incomes: {3}",
+                this.Id, this.ProductName, this.VendorName, this.Quantity,this.Incomes);
+        }
+    }
+}
