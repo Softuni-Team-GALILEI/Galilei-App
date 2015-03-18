8204461c77493b38c928fef5bf1ebd1a92ee8a25
AKJ1 <kellerr@abv.bg>
2015-03-18 21:31:04 +0200

    Finished Merge

---
+++ MsSqlToPDF/Main.cs
@@ -0,0 +1,12 @@
+﻿namespace MsSqlToPDF
+{
+    using System;
+    using SalesReports;
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            PdfExpenses.CreatePdfSalesReport(new DateTime(2014, 7, 20), new DateTime(2014, 7, 22));
+        }
+    }
+}
